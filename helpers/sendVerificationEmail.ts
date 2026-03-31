9416926653bea12eacd09c5bac4429b444b31b8e
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-03-31 19:55:22 +1030

    feat(auth): add email verification service using resend

---
+++ helpers/sendVerificationEmail.ts
@@ -0,0 +1,23 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "@/emails/VerificationEmail";
+
+
+export async function sendVerificationEmail(
+    email:string,
+    username:string,
+    verifyCode:string,
+){
+    try{
+        await resend.emails.send({
+            from:'Acme <onboarding@resend.dev>',
+             to: email,
+            subject: 'Mystery Message Verification Code',
+            react: VerificationEmail({ username, otp: verifyCode }),
+        })
+        return {success:true, message:'Verification email '}
+    }
+    catch(emailError){
+        console.error('Error sending verification email:', emailError);
+    return { success: false, message: 'Failed to send verification email.' };
+    }
+}
