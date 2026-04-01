52bccc0ae21da25094db0eacdd1f73445ecd57b0
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-04-01 20:12:00 +1030

    use data.message for clearer API error handling

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -14,7 +14,7 @@ export async function apiClient(
     const data = await res.json()
 
     if (!res.ok){
-        throw new Error (data || "Something went wrong")
+        throw new Error (data.message || "Something went wrong")
     }
 
     return data
