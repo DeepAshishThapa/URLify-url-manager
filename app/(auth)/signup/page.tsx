26aeb00986fafdfc01a128619b4a500a691ccd0f
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-04-07 21:50:21 +0930

    refactor(auth): returned null for unused page after migrating to OAuth

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,13 +1,7 @@
-import Signup from "@/components/Signup"
+
 
 function page() {
-    return (
-        <div className='w-full h-[90vh] flex justify-center items-center'>
-            <Signup/>
-            
-
-        </div>
-    )
+    return null
 }
 
 export default page
