7907acfc06ec41cec499a25c4f1577a21066a425
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-04-11 18:22:46 +0930

    feat: add Link model with nesessary fields

---
+++ model/link.model.ts
@@ -0,0 +1,50 @@
+import mongoose,{Schema} from "mongoose"
+
+
+export interface LinkObject extends mongoose.Document{
+    url: string,
+    description?: string,
+    folderId?: mongoose.Types.ObjectId,
+    userId: mongoose.Types.ObjectId
+
+}
+
+const linkSchema: Schema<LinkObject> = new Schema(
+    {
+        url:{
+            type:String,
+            required:[true, 'url is required'],
+            trim: true
+
+        },
+        description:{
+            type:String,
+            trim: true 
+        },
+        userId:{
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required:true
+        },
+        folderId:{
+            type: Schema.Types.ObjectId,
+            ref: "Folder",
+            required: false,
+            default: null
+
+        }
+
+
+    },
+    { timestamps: true }
+)
+    
+const linkModel = (mongoose.models.link as mongoose.Model<LinkObject>) ||
+    mongoose.model<LinkObject>('Link', linkSchema );
+
+export default linkModel
+
+
+
+
+
