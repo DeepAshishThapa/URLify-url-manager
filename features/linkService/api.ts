3e1221d34a86a5dd79a5f40a1fa703d46c6327a9
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-05-01 21:33:20 +0930

    feat: add getLinksByFolder and deleteLink API functions

--- features/linkService/api.ts
+++ features/linkService/api.ts
@@ -22,3 +22,17 @@ export async function createLink({url, description, folderId}:CreateLinkPayload)
     })
 
 }
+
+//get links by folder
+export async function getLinksByFolder (folderId:string) {
+     return apiClient(`${BASE}?folderId=${folderId}`)
+}
+
+export async function deleteLink(id:string){
+    return apiClient(`${BASE}/${id}`, {
+         method: "DELETE",
+    }
+
+
+    )
+}
