a520013573670a7f712bae39d0faffa9b44d4658
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-05-02 18:32:47 +0930

    feat: add API function to fetch unsaved links

--- features/linkService/api.ts
+++ features/linkService/api.ts
@@ -1,38 +1,42 @@
 import { apiClient } from "@/lib/apiClient";
 
 type CreateLinkPayload = {
-  url: string
-  description?: string
-  folderId?: string | null
+    url: string
+    description?: string
+    folderId?: string | null
 }
 
-const BASE= "/api/link"
+const BASE = "/api/link"
 
 // Get All
-export async function getAllLinks(){
+export async function getAllLinks() {
     return apiClient(BASE)
 
 }
 
 // Create
-export async function createLink({url, description, folderId}:CreateLinkPayload){
+export async function createLink({ url, description, folderId }: CreateLinkPayload) {
     return apiClient(BASE, {
         method: "POST",
-        body: JSON.stringify({url, description, folderId})
+        body: JSON.stringify({ url, description, folderId })
     })
 
 }
 
 //get links by folder
-export async function getLinksByFolder (folderId:string) {
-     return apiClient(`${BASE}?folderId=${folderId}`)
+export async function getLinksByFolder(folderId: string) {
+    return apiClient(`${BASE}?folderId=${folderId}`)
 }
 
-export async function deleteLink(id:string){
+export async function getUnsavedLinks() {
+    return apiClient(`${BASE}?type=unsaved`);
+}
+
+
+export async function deleteLink(id: string) {
     return apiClient(`${BASE}/${id}`, {
-         method: "DELETE",
+        method: "DELETE",
     }
-
-
-    )
+   )
 }
+
